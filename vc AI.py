21aad6bd788f86f204936ea51ab6110faac1b975
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-04-07 19:41:47 +0530

    Initial commit

---
+++ vc AI.py
@@ -0,0 +1,125 @@
+import pyttsx3 
+import speech_recognition as sr 
+import datetime
+import wikipedia 
+import webbrowser
+import os
+import smtplib
+import pandas  as pd
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am timimm. Please tell me how may I help you")       
+
+def takeCommand():
+   
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+           
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+  
+        query = takeCommand().lower()
+
+        
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in quer
+         
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+
+        elif 'play video' in query:
+            video_dir = "C:\\Users\\BIMAL RAY\\Desktop\\capture\\Valorant"
+            videos = os.listdir(video_dir)
+            print(videos)    
+            os.startfile(os.path.join(video_dir, videos[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\bimal\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+        
+        elif 'sequence it' in query:
+            ss = pd.read_csv( "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data",names=["sepal length", "sepal width", "petal length", "petal width" , "class" ] )
+
+            print(ss)
+
+        elif 'fuck off' in query:
+            speak("pardon me sir")
+         
+
+        elif 'email to vimal' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "bimal.ray99@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend bimal bhai. I am not able to send this email")    
+        
+        elif 'stop' in query:
+            speak("thank you sir")
+        break
+
